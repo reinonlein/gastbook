8ab2703a02ad7bee4322b38001a818dabd183d14
Rein Kloos <kloos_r@schiphol.nl>
2025-01-12 15:10:47 +0100

    Fix PATH issue for Flutter in Vercel

--- install_flutter.sh
+++ install_flutter.sh
@@ -11,7 +11,10 @@ echo "Cloning Flutter repository..."
 git clone --depth 1 --branch stable https://github.com/flutter/flutter.git /usr/local/flutter
 
 # 2. Voeg Flutter toe aan het PATH
+echo "Adding Flutter to PATH..."
 export PATH="$PATH:/usr/local/flutter/bin"
+echo 'export PATH="$PATH:/usr/local/flutter/bin"' >> ~/.bashrc
+echo 'export PATH="$PATH:/usr/local/flutter/bin"' >> ~/.profile
 
 # 3. Update Flutter dependencies
 echo "Running 'flutter doctor' to download dependencies..."
